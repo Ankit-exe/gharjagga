6302eb244539e1c07a4308d1a514111afa1360d1
Ankit <ankitghimire321@gmail.com>
2023-10-15 08:52:01 +0545

    errorr free

---
+++ api/routes/user.route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/test',(req,res)=>
+{
+    res.json({
+        message:'Hello World',
+    });
+})
+
+export default router;
