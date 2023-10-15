c0b41f3ccec4e99705bfeec4abc9a20bca7d6e82
Ankit <ankitghimire321@gmail.com>
2023-10-15 08:40:13 +0545

    user models

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const dbconfig = require("./config/dbconfig");
+const dbconfig = require("./server/config/dbconfig");
 
 app.listen(port, () => console.log(`NODE JS SERVER IS STARTED AT ${port}`));
