f714eeaefcd283bb145f8e928253fa8c48205090
Ankit <ankitghimire321@gmail.com>
2023-10-20 09:12:54 +0545

    update header profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from './pages/SignIn'
 import About from './pages/About'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import PrivateRoute from './components/privateRoute'
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route element={<PrivateRoute />} >
       <Route path="/profile" element={<Profile />} />
+      </Route>
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
