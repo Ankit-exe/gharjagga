c5a611dfa0c177c537f11cf2fda30c683678f6ed
Ankit <ankitghimire321@gmail.com>
2023-11-01 17:42:13 +0545

    search api routes

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -48,17 +48,61 @@ const updateListing = async (req, res, next) => {
   }
 };
 
-const getListing = async (req, res, next) =>
-{
-      try {
-        const listing = await Listing.findById(req.params.id);
-          if (!listing) {
-            return next(errorHandler(404, "Listing not found!"));
-          }
-          res.status(200).json(listing);
-      } catch (error) {
-        next(error);
-      }
+const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.limit) || 0;
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, undefined] };
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listing = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({
+        [sort]: order,
+      })
+      .limit(limit)
+      .skip(startIndex);
+    return res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
@@ -66,4 +110,5 @@ module.exports = {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 };
