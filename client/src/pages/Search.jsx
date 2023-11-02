0dfecaadb8433e5110417835ff5d1da78611c2a3
Ankit <ankitghimire321@gmail.com>
2023-11-02 08:34:19 +0545

    fetching error solved(complete search funct...)

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,88 +1,92 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Search() {
   const navigate = useNavigate();
   const [sidebardata, setSidebardata] = useState({
-    searchTerm: "",
-    type: "all",
+    searchTerm: '',
+    type: 'all',
     parking: false,
     furnished: false,
     offer: false,
-    sort: "created_at",
-    order: "desc",
+    sort: 'created_at',
+    order: 'desc',
   });
+
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
   console.log(listings);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const searchTermFormUrl = urlParams.get("searchTerm");
-    const typeFromUrl = urlParams.get("type");
-    const parkingFormUrl = urlParams.get("parking");
-    const furnishedFormUrl = urlParams.get("furnished");
-    const offerFormUrl = urlParams.get("offer");
-    const sortFormUrl = urlParams.get("sort");
-    const orderFormUrl = urlParams.get("order");
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    const typeFromUrl = urlParams.get('type');
+    const parkingFromUrl = urlParams.get('parking');
+    const furnishedFromUrl = urlParams.get('furnished');
+    const offerFromUrl = urlParams.get('offer');
+    const sortFromUrl = urlParams.get('sort');
+    const orderFromUrl = urlParams.get('order');
 
     if (
-      searchTermFormUrl ||
+      searchTermFromUrl ||
       typeFromUrl ||
-      parkingFormUrl ||
-      furnishedFormUrl ||
-      offerFormUrl ||
-      sortFormUrl ||
-      orderFormUrl
+      parkingFromUrl ||
+      furnishedFromUrl ||
+      offerFromUrl ||
+      sortFromUrl ||
+      orderFromUrl
     ) {
       setSidebardata({
-        searchTerm: searchTermFormUrl || "",
-        type: typeFromUrl || "all",
-        parking: parkingFormUrl === "true" ? true : false,
-        furnished: furnishedFormUrl === "true" ? true : false,
-        offer: offerFormUrl === "true" ? true : false,
-        sort: sortFormUrl || "created_at",
-        order: orderFormUrl || "desc",
+        searchTerm: searchTermFromUrl || '',
+        type: typeFromUrl || 'all',
+        parking: parkingFromUrl === 'true' ? true : false,
+        furnished: furnishedFromUrl === 'true' ? true : false,
+        offer: offerFromUrl === 'true' ? true : false,
+        sort: sortFromUrl || 'created_at',
+        order: orderFromUrl || 'desc',
       });
     }
 
-    const fetchlisting = async () => {
+    const fetchListings = async () => {
       setLoading(true);
       const searchQuery = urlParams.toString();
-      const res = await fetch(`/api/listing/get?${searchQuery}}`);
+      const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
       setListings(data);
       setLoading(false);
     };
-    fetchlisting();
+    fetchListings();
   }, [location.search]);
 
   const handleChange = (e) => {
     if (
-      e.target.id === "all" ||
-      e.target.id === "rent" ||
-      e.target.id === "sale"
+      e.target.id === 'all' ||
+      e.target.id === 'rent' ||
+      e.target.id === 'sale'
     ) {
       setSidebardata({ ...sidebardata, type: e.target.id });
     }
-    if (e.target.id === "searchTerm") {
+
+    if (e.target.id === 'searchTerm') {
       setSidebardata({ ...sidebardata, searchTerm: e.target.value });
     }
+
     if (
-      e.target.id === "parking" ||
-      e.target.id === "furnished" ||
-      e.target.id === "offer"
+      e.target.id === 'parking' ||
+      e.target.id === 'furnished' ||
+      e.target.id === 'offer'
     ) {
       setSidebardata({
         ...sidebardata,
         [e.target.id]:
-          e.target.checked || e.target.checked === "true" ? true : false,
+          e.target.checked || e.target.checked === 'true' ? true : false,
       });
     }
-    if (e.target.id === "sort_order") {
-      const sort = e.target.value.split("_")[0] || "created_at";
 
-      const order = e.target.value.split("_")[1] || "desc";
+    if (e.target.id === 'sort_order') {
+      const sort = e.target.value.split('_')[0] || 'created_at';
+
+      const order = e.target.value.split('_')[1] || 'desc';
 
       setSidebardata({ ...sidebardata, sort, order });
     }
@@ -90,18 +94,18 @@ export default function Search() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     const urlParams = new URLSearchParams();
-    urlParams.set("searchTerm", sidebardata.searchTerm);
-    urlParams.set("type", sidebardata.type);
-    urlParams.set("parking", sidebardata.parking);
-    urlParams.set("furnished", sidebardata.furnished);
-    urlParams.set("offer", sidebardata.offer);
-    urlParams.set("sort", sidebardata.sort);
-    urlParams.set("order", sidebardata.order);
+    urlParams.set('searchTerm', sidebardata.searchTerm);
+    urlParams.set('type', sidebardata.type);
+    urlParams.set('parking', sidebardata.parking);
+    urlParams.set('furnished', sidebardata.furnished);
+    urlParams.set('offer', sidebardata.offer);
+    urlParams.set('sort', sidebardata.sort);
+    urlParams.set('order', sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
+
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen font-normal">
@@ -189,15 +193,14 @@ export default function Search() {
             <label className=" font-medium">Sort:</label>
             <select
               onChange={handleChange}
-              defaultValue={"created_at_desc"}
-              name=""
-              id="sort_order"
-              className="border rounded-lg p-3"
+              defaultValue={'created_at_desc'}
+              id='sort_order'
+              className='border rounded-lg p-3'
             >
-              <option value="regularPrice_desc">Price High to Low</option>
-              <option value="regularPrice_asc">Price Low to High</option>
-              <option value="createdAt_desc">Latest</option>
-              <option value="createdAt_asc">Oldest</option>
+              <option value='regularPrice_desc'>Price high to low</option>
+              <option value='regularPrice_asc'>Price low to high</option>
+              <option value='createdAt_desc'>Latest</option>
+              <option value='createdAt_asc'>Oldest</option>
             </select>
           </div>
           <button className="bg-green-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
@@ -213,3 +216,6 @@ export default function Search() {
     </div>
   );
 }
+
+
+
