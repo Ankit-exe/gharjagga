a7a6582a78ee85ab9f539dbbcc1a793c0345c845
Ankit <ankitghimire321@gmail.com>
2023-11-02 19:23:18 +0545

    create listing item component and showin all list

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const verifyToken = require('../utils/verifyuser.js');
+const express = require("express");
+const verifyToken = require("../utils/verifyuser.js");
 const {
   createListing,
   deleteListing,
@@ -10,10 +10,10 @@ const {
 
 const router = express.Router();
 
-router.post('/create', verifyToken ,createListing );
-router.delete('/delete/:id' , verifyToken , deleteListing);
-router.post('/update/:id', verifyToken , updateListing);
-router.get('/get/:id', getListing);
-router.get('/get',getListings);
+router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 module.exports = router;
