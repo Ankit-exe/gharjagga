060893a2caebc760372d7446d71b46f732075e6d
Ankit <ankitghimire321@gmail.com>
2023-11-03 12:53:09 +0545

    home and about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-green-800">About GharJhagga</h1>
+      <p className=" mb-4 text-green-700">
+        Welcome to GharJagga, where we're passionate about helping you find your
+        ideal home, property, or investment opportunity. Our journey began with
+        a vision to simplify and elevate the real estate experience for both
+        buyers and sellers. We believe that finding the perfect space should be
+        an exciting and seamless process, and our dedicated team is here to make
+        that vision a reality.
+      </p>
+      <p className=" mb-4 text-green-700">
+        At GharJagga, we are more than just a real estate platform; we are your
+        trusted partner in the world of property transactions. With a wealth of
+        experience and a deep understanding of the local real estate market, we
+        offer you a comprehensive range of listings, expert insights, and
+        professional guidance. Whether you're searching for your dream home,
+        looking to invest, or seeking to sell your property, our mission is to
+        provide you with the tools, resources, and expertise to make informed
+        decisions.
+      </p>
+      <p className=" mb-4 text-green-700">
+        Our commitment to transparency, integrity, and customer satisfaction is
+        at the heart of what we do. We believe that your real estate journey
+        should be marked by trust, reliability, and a personalized touch. Thank
+        you for choosing GharJagga as your real estate destination. We look
+        forward to accompanying you on your path to finding the perfect "ghar"
+        (home) or "jagga" (property), and we're excited to be part of your real
+        estate success story.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
