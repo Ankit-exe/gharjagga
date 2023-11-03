0b6a955f6aea9904b5aa350d1f11dc39af4f652f
Ankit <ankitghimire321@gmail.com>
2023-11-03 13:41:11 +0545

    backend fronted done,twerks to deploy on render

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+
+const __dirname = path.resolve();
+
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cookieParser());
+const path = require('path');
 
 const dbconfig = require("./config/dbconfig");
 const userRouter = require("./routes/user.route");
@@ -12,11 +16,19 @@ const authRouter = require("./routes/auth.route");
 const createListing = require("./routes/listing.route");
 
 
+app.listen(port, () => console.log(`NODE JS SERVER IS STARTED AT ${port}`));
+
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", createListing);
 
-app.listen(port, () => console.log(`NODE JS SERVER IS STARTED AT ${port}`));
+app.use(express.static(path.join(__dirname , '/client/dist')));
+
+app.get('*' ,(req,res) =>
+{
+  res.sendFile(path.join(__dirname,'client' , 'dist' , 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
