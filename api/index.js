0ad1a3aa1b7ce21c1672eae8db503bb8e6df3f87
Ankit <88811553+Ankit-exe@users.noreply.github.com>
2023-11-04 18:31:31 +0545

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,32 +1,33 @@
-const express = require("express");
-
-const app = express();
-const port = process.env.PORT || 5000;
-require("dotenv").config();
-const cookieParser = require("cookie-parser");
-app.use(express.json());
-app.use(cookieParser());
-const path = require('path');
-
-const dbconfig = require("./config/dbconfig");
 const userRouter = require("./routes/user.route");
 const authRouter = require("./routes/auth.route");
-const createListing = require("./routes/listing.route");
+const listingRouter = require("./routes/listing.route");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const express = require("express");
+require("dotenv").config();
+
+const dbconfig = require("./config/dbconfig");
 
 
-app.listen(port, () => console.log(`NODE JS SERVER IS STARTED AT ${port}`));
+const app = express();
 
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/listing", createListing);
+app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname , '/client/dist')));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get('*' ,(req,res) =>
-{
-  res.sendFile(path.join(__dirname,'client' , 'dist' , 'index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
